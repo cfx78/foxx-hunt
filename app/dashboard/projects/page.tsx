f7b36fd5f0f6a6303cdf4306c29922be85c026b3
cfx78 <104265674+cfx78@users.noreply.github.com>
2023-10-23 19:36:20 -0500

    removing console log

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -6,7 +6,6 @@ import ProjectsPageFunctions from '@/lib/ServerComponentFunctions/MainPages/Proj
 
 const Projects = async () => {
 	const data = await ProjectsPageFunctions();
-	console.log(data.userProjects);
 
 	return (
 		<div className='projects-container'>
